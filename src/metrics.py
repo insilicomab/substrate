9665f4124850ecde29f148bb6b198411a82a0428
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2022-09-22 00:36:05 +0900

    Create metrics.py

---
+++ src/metrics.py
@@ -0,0 +1,37 @@
+import torchmetrics
+from omegaconf import DictConfig
+
+
+def get_metrics(cfg: DictConfig):
+    metrics = torchmetrics.MetricCollection(
+            [
+                torchmetrics.Accuracy(),
+                torchmetrics.Precision(
+                    num_classes=cfg.num_classes,
+                    average=cfg.metrics.precision.average,
+                ),
+                torchmetrics.Recall(
+                    num_classes=cfg.num_classes,
+                    average=cfg.metrics.recall.average,
+                ),
+                torchmetrics.Specificity(
+                    num_classes=cfg.num_classes,
+                    average=cfg.metrics.specificity.average,
+                ),
+                torchmetrics.F1Score(
+                    num_classes=cfg.num_classes,
+                    average=cfg.metrics.f1.average,
+                ),
+                torchmetrics.FBetaScore(
+                    num_classes=cfg.num_classes,
+                    beta=cfg.metrics.f_beta.beta,
+                    average=cfg.metrics.f_beta.average,
+                ),
+                torchmetrics.AUROC(
+                    num_classes=cfg.num_classes,
+                    average=cfg.metrics.auroc.average,
+                )
+            ]
+        )
+
+    return metrics
